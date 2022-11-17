5318e22bb3b7241e0cd56fe7f7289ed08a573cef
Lana243 <lana0771711@gmail.com>
2022-11-17 12:46:57 +0300

    add tests for simulator

--- src/test/kotlin/roguelike/SimulatorTest.kt
+++ src/test/kotlin/roguelike/SimulatorTest.kt
@@ -1,22 +1,34 @@
 package roguelike
 
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
+import roguelike.state.game.simulator.Interact
 import roguelike.state.game.simulator.MoveAction
 import roguelike.state.game.simulator.Procrastinate
 import roguelike.state.game.simulator.SimulatorImpl
 import roguelike.state.game.world.MapFromFileGenerator
 import roguelike.state.game.world.Position
+import roguelike.state.game.world.World
 import roguelike.state.game.world.WorldFactory
 import roguelike.state.game.world.objects.units.PlayerUnit
 
 class SimulatorTest {
-    @Test
-    fun `If player moves to the wall, his position does not change`() {
+
+    lateinit var world: World
+    lateinit var simulator: SimulatorImpl
+
+    @BeforeEach
+    fun initWorld() {
         val mapFactory = MapFromFileGenerator("src/test/resources/test-map.txt")
         val worldFactory = WorldFactory(mapFactory)
-        var world = worldFactory.createWorld()
-        val simulator = SimulatorImpl()
+        world = worldFactory.createWorld()
+        simulator = SimulatorImpl()
+    }
+
+    @Test
+    fun `If player moves to the wall, his position does not change`() {
         world = simulator.simulate(world) {
             when (it) {
                 is PlayerUnit -> {
@@ -27,6 +39,81 @@ class SimulatorTest {
                 }
             }
         }
-        Assertions.assertTrue(world.player.position == Position(1, 2))
+        Assertions.assertEquals(Position(1, 2), world.player.position)
+    }
+
+    @Test
+    fun `If player goes to cell with apple, his health increases`() {
+        Assertions.assertEquals(50, world.player.hp)
+        repeat(3) {
+            world = simulator.simulate(world) {
+                when (it) {
+                    is PlayerUnit -> {
+                        MoveAction.DOWN
+                    }
+                    else -> {
+                        Procrastinate
+                    }
+                }
+            }
+        }
+        Assertions.assertEquals(60, world.player.hp)
+    }
+
+    @Disabled
+    @Test
+    fun `If player goes to well and interacts, his level increments`() {
+        Assertions.assertEquals(1, world.player.level)
+        repeat(9) {
+            world = simulator.simulate(world) {
+                when (it) {
+                    is PlayerUnit -> {
+                        MoveAction.RIGHT
+                    }
+                    else -> {
+                        Procrastinate
+                    }
+                }
+            }
+        }
+        world = simulator.simulate(world) {
+            when (it) {
+                is PlayerUnit -> {
+                    Interact
+                }
+                else -> {
+                    Procrastinate
+                }
+            }
+        }
+        Assertions.assertEquals(2, world.player.level)
+    }
+
+    @Test
+    fun `If player goes to the cell with well, his position does not change`() {
+        repeat(9) {
+            world = simulator.simulate(world) {
+                when (it) {
+                    is PlayerUnit -> {
+                        MoveAction.RIGHT
+                    }
+                    else -> {
+                        Procrastinate
+                    }
+                }
+            }
+        }
+        Assertions.assertEquals(Position(11, 2), world.player.position)
+        world = simulator.simulate(world) {
+            when (it) {
+                is PlayerUnit -> {
+                    MoveAction.RIGHT
+                }
+                else -> {
+                    Procrastinate
+                }
+            }
+        }
+        Assertions.assertEquals(Position(11, 2), world.player.position)
     }
 }
